a440daede7e351cf40c87154b2563540802ee594
eubank87 <eubank.ashley@gmail.com>
2021-01-19 18:34:24 -0800

    restructured schema file so it read top to bottom

--- trackerSchema.sql
+++ trackerSchema.sql
@@ -3,25 +3,25 @@ CREATE DATABASE tracker_db;
 
 USE tracker_db;
 
-CREATE TABLE employee(
-  id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(45) NOT NULL,
-  last_name VARCHAR(45) NOT NULL,
-  role_id INTEGER NOT NULL,
-  manager_id INTEGER,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+CREATE TABLE department(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE role(
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE roles(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     title VARCHAR(45) NOT NULL,
-    salary INTEGER NOT NULL,
-    department_id INTEGER NOT NULL,
+    salary INT NOT NULL,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE department(
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(45) NOT NULL
-)
+CREATE TABLE employee(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(45) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
