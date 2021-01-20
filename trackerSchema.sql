46598fc0068094df5228c49c8602fbf1c2af0f15
eubank87 <eubank.ashley@gmail.com>
2021-01-19 18:06:50 -0800

    installed npm packages, created schema page with initial db

---
+++ trackerSchema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS tracker_DB;
+CREATE DATABASE tracker_DB;
+
+USE tracker_DB;
+
+CREATE TABLE employee(
+  id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
+CREATE TABLE role(
+    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title VARCHAR(45) NOT NULL,
+    salary INTEGER NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE department(
+    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL
+)
